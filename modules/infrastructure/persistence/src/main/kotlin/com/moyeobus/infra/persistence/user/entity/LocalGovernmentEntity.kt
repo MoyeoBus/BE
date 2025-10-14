e5016caac581de7d83a132a49d820c3aa0f16bdc
Jinyoung <glitt5384@naver.com>
2025-10-14 21:17:29 +0900

    feat: 지자체 사용자 엔티티, 포트, 어댑터

---
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/user/entity/LocalGovernmentEntity.kt
@@ -0,0 +1,17 @@
+package com.moyeobus.infra.persistence.user.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "local_goverment")
+class LocalGovernmentEntity (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+    var loginId: String,
+    var password: String,
+    var govName: String
+)
