bd21b31c75e45c394bd00ddaf99a28bfcbf8c4b0
Jinyoung <glitt5384@naver.com>
2025-10-17 23:58:00 +0900

    feat: 쿼리 응답 객체

--- modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/route/dto/QueryDtoList.kt
+++ modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/route/dto/QueryDtoList.kt
@@ -2,6 +2,14 @@ package com.moyeobus.api.route.dto
 
 data class QueryResponse(
     val items: List<RouteRequestResponse>,
+    val summary: Summary,
     val nextCursor: String?,
     val hasNext: Boolean,
+)
+
+data class Summary(
+    val totalCount: Long,
+    val approvedCount: Long,
+    val cancelledCount: Long,
+    val pendingCount: Long
 )
