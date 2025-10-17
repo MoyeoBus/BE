7368a278526a89e108f68ce8aa853d2bebfc7572
Jinyoung <glitt5384@naver.com>
2025-10-17 15:29:34 +0900

    feat: 컴포넌트 어노테이션 추가

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/user/adapter/TransportOperatorPersistenceAdapter.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/user/adapter/TransportOperatorPersistenceAdapter.kt
@@ -1,6 +1,8 @@
 package com.moyeobus.infra.persistence.user.adapter
 
 import com.moyeobus.application.user.port.out.TransportOperatorOutPort
+import org.springframework.stereotype.Component
 
+@Component
 class TransportOperatorPersistenceAdapter : TransportOperatorOutPort {
 }
