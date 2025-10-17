1b1bcf8046b8d6d9c01e7ccdff15f5080f398f53
Jinyoung <glitt5384@naver.com>
2025-10-17 15:29:21 +0900

    feat: enablejpaauditing

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/config/JpaConfig.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/config/JpaConfig.kt
@@ -2,6 +2,7 @@ package com.moyeobus.infra.persistence.config
 
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
  * - 리포지토리/엔티티 패키지를 스캔하여 Spring Data JPA 구성을 활성화합니다.
  */
 @Configuration
+@EnableJpaAuditing
 @EnableJpaRepositories(basePackages = ["com.moyeobus.infra.persistence"])
 @EntityScan(basePackages = ["com.moyeobus.infra.persistence"])
 class JpaConfig
