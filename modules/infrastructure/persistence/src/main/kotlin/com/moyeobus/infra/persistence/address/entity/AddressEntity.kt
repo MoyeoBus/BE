60db7aa1e9259d9be9b9223c2179c8e853377f3a
Jinyoung <glitt5384@naver.com>
2025-10-22 13:12:03 +0900

    feat: 주소 영속성 엔티티

---
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/address/entity/AddressEntity.kt
@@ -0,0 +1,32 @@
+package com.moyeobus.infra.persistence.address.entity
+
+import com.moyeobus.global.entity.BaseEntity
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "address")
+class AddressEntity (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @ManyToOne(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    @JoinColumn(name = "area_id")
+    val area: AreaEntity,
+
+    val name: String,
+
+    val lat: Double,
+
+    val lon: Double,
+
+    val postCode: String
+) : BaseEntity()
