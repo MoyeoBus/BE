8c50b43faad3740230eeefa23596bf36e09a91e0
Jinyoung <glitt5384@naver.com>
2025-10-25 23:18:09 +0900

    feat: 노선 거리 및 예상 소요 시간 속성 추가

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/route/entity/RouteEntity.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/route/entity/RouteEntity.kt
@@ -1,10 +1,12 @@
 package com.moyeobus.infra.persistence.route.entity
 
 import com.moyeobus.global.entity.BaseEntity
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 
 @Entity
@@ -15,5 +17,12 @@ class RouteEntity (
 
     val operatorId: Long,
 
-    val localGovId: Long
+    val localGovId: Long,
+
+    val routeDistance: Double,
+
+    val routeTotalTime: Double,
+
+    @OneToMany(mappedBy = "route", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val routeComponents: List<RouteComponentEntity>
 ) : BaseEntity()
