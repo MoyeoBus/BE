1a8fde4d2444630e6ab5f3e0e4013f5f9dd374f9
Jinyoung <glitt5384@naver.com>
2025-10-27 16:19:08 +0900

    feat: 운수사 도메인 DB 상호작용

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/user/adapter/TransportOperatorPersistenceAdapter.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/user/adapter/TransportOperatorPersistenceAdapter.kt
@@ -1,8 +1,15 @@
 package com.moyeobus.infra.persistence.user.adapter
 
 import com.moyeobus.application.user.port.out.TransportOperatorOutPort
+import com.moyeobus.domain.user.TransportOperator
+import com.moyeobus.infra.persistence.user.repository.TransportOperatorJpaRepository
 import org.springframework.stereotype.Component
 
 @Component
-class TransportOperatorPersistenceAdapter : TransportOperatorOutPort {
+class TransportOperatorPersistenceAdapter(
+    private val repo: TransportOperatorJpaRepository
+) : TransportOperatorOutPort {
+    override fun findById(id: Long): TransportOperator {
+        TODO("Not yet implemented")
+    }
 }
