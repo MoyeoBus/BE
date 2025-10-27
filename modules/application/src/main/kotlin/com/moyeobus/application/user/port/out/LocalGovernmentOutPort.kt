97c7fa25735f46ef0cef0e24e0ec9431e05b4380
Jinyoung <glitt5384@naver.com>
2025-10-27 16:16:57 +0900

    feat: 지자체 도메인 DB 상호작용

--- modules/application/src/main/kotlin/com/moyeobus/application/user/port/out/LocalGovernmentOutPort.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/user/port/out/LocalGovernmentOutPort.kt
@@ -1,4 +1,7 @@
 package com.moyeobus.application.user.port.out
 
+import com.moyeobus.domain.user.LocalGovernment
+
 interface LocalGovernmentOutPort {
+    fun findById(id: Long) : LocalGovernment
 }
