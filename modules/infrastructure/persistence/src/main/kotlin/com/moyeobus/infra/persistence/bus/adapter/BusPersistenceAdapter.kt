33a229e2a3644b39ea41cdd54ed65650e1834798
Jinyoung <glitt5384@naver.com>
2025-10-29 22:06:36 +0900

    feat: 버스 도메인의 영속성 어댑터

---
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/bus/adapter/BusPersistenceAdapter.kt
@@ -0,0 +1,8 @@
+package com.moyeobus.infra.persistence.bus.adapter
+
+import com.moyeobus.application.bus.port.out.BusOutPort
+import org.springframework.stereotype.Component
+
+@Component
+class BusPersistenceAdapter : BusOutPort {
+}
