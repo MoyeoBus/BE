e637f7a6e62fa479e791978d621dd0405d50a693
Jinyoung <glitt5384@naver.com>
2025-10-30 22:43:15 +0900

    feat: 버스 시드 데이터 삽입

---
+++ modules/infrastructure/persistence/src/main/resources/db/migration/V6__seed_bus_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO bus (bus_number, car_number, operator_id, status)
+VALUES
+(101, '11가1234', 1, 'IDLE'),
+(102, '11가5678', 1, 'OPERATING'),
+(201, '22나1111', 2, 'IDLE'),
+(202, '22나2222', 2, 'OPERATING'),
+(301, '33다3333', 3, 'OPERATING'),
+(302, '33다4444', 3, 'IDLE'),
+(401, '44라5555', 4, 'OPERATING'),
+(402, '44라6666', 4, 'IDLE'),
+(501, '55마7777', 5, 'OPERATING'),
+(502, '55마8888', 5, 'IDLE');
