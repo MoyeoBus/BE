735a8f61e680a9ff8b1370054f2ab4abcf85b4c0
Jinyoung <glitt5384@naver.com>
2025-10-31 23:56:07 +0900

    feat: 유효성 검사 추가

--- modules/application/src/main/kotlin/com/moyeobus/application/route/port/in/RouteCommand.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/route/port/in/RouteCommand.kt
@@ -1,10 +1,15 @@
 package com.moyeobus.application.route.port.`in`
 
+import jakarta.validation.constraints.NotNull
 import java.time.LocalDateTime
 
 data class RouteCommand(
+    @field:NotNull(message = "출발지 ID는 필수 값입니다.")
     val departureId: Long,
+    @field:NotNull(message = "도착지 ID는 필수 값입니다.")
     val destinationId: Long,
+    @field:NotNull(message = "출발 시각은 필수 값입니다.")
     val startDateTime: LocalDateTime,
+    @field:NotNull(message = "도착 시각은 필수 값입니다.")
     val endDateTime: LocalDateTime
 )
