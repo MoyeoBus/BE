a05b377ef5ee733bb1fd7db463cbd9687808e498
Jinyoung <glitt5384@naver.com>
2025-11-04 23:30:39 +0900

    feat: 설문조사 응답의 영속성, 도메인 엔티티

---
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/survey/entity/SurveyAnswerEntity.kt
@@ -0,0 +1,20 @@
+package com.moyeobus.infra.persistence.survey.entity
+
+import com.moyeobus.global.entity.BaseEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+
+@Entity
+@Table(name = "survey_answer")
+class SurveyAnswerEntity(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    val departureLocalGovId: Long,
+
+    val destinationLocalGovId: Long,
+) : BaseEntity()
