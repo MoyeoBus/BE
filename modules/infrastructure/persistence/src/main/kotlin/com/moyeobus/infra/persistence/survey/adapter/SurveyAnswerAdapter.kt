0ae52a039286809210ce3a72cd8d27fdc74addfb
Jinyoung <glitt5384@naver.com>
2025-11-04 23:30:44 +0900

    feat: 설문조사 응답 어댑터

---
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/survey/adapter/SurveyAnswerAdapter.kt
@@ -0,0 +1,18 @@
+package com.moyeobus.infra.persistence.survey.adapter
+
+import com.moyeobus.application.survey.port.out.SurveyAnswerOutPort
+import com.moyeobus.domain.survey.SurveyAnswer
+import com.moyeobus.infra.persistence.survey.mapper.SurveyAnswerMapper
+import com.moyeobus.infra.persistence.survey.repository.SurveyAnswerJpaRepository
+import org.springframework.stereotype.Component
+
+@Component
+class SurveyAnswerAdapter(
+    private val mapper: SurveyAnswerMapper,
+    private val repo: SurveyAnswerJpaRepository
+) : SurveyAnswerOutPort{
+    override fun save(answer: SurveyAnswer) {
+        repo.save(mapper.toEntity(answer))
+    }
+
+}
