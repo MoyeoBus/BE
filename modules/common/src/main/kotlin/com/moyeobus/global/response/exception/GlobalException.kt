aca0bc34fc3ff5a47f30feaf48a2fe82a47ec8d8
Jinyoung <glitt5384@naver.com>
2025-11-05 16:32:44 +0900

    fix: open -> data, private 제거

--- modules/common/src/main/kotlin/com/moyeobus/global/response/exception/GlobalException.kt
+++ modules/common/src/main/kotlin/com/moyeobus/global/response/exception/GlobalException.kt
@@ -4,7 +4,7 @@ import com.moyeobus.global.response.BaseStatusCode
 import com.moyeobus.global.response.ErrorDetail
 
 
-data class GlobalException(private val code: BaseStatusCode) : RuntimeException(code.message) {
+open class GlobalException(val code: BaseStatusCode) : RuntimeException(code.message) {
     val errorDetail: ErrorDetail
         get() = ErrorDetail.Companion.from(this.code)
 }
