d11eba8de2bee03ee6eced842835c414677cbeea
Jinyoung <glitt5384@naver.com>
2025-11-05 16:31:29 +0900

    feat: ? 제거

--- modules/common/src/main/kotlin/com/moyeobus/global/response/ErrorDetail.kt
+++ modules/common/src/main/kotlin/com/moyeobus/global/response/ErrorDetail.kt
@@ -3,9 +3,9 @@ package com.moyeobus.global.response
 import org.springframework.http.HttpStatus
 
 data class ErrorDetail(
-    val httpStatus: HttpStatus?,
-    val code: String?,
-    val message: String?
+    val httpStatus: HttpStatus,
+    val code: String,
+    val message: String
 ) {
     companion object {
         fun from(code: BaseStatusCode): ErrorDetail {
