ff737ae159c2a6110e39d54252882d9d181a9afd
Jinyoung <glitt5384@naver.com>
2025-11-05 22:03:39 +0900

    chore: mapping 의존성 제거

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/survey/mapper/SurveyOptionMapper.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/survey/mapper/SurveyOptionMapper.kt
@@ -3,7 +3,6 @@ package com.moyeobus.infra.persistence.survey.mapper
 import com.moyeobus.domain.survey.SurveyOption
 import com.moyeobus.infra.persistence.survey.entity.SurveyOptionEntity
 import org.mapstruct.Mapper
-import org.mapstruct.Mapping
 
 @Mapper(componentModel = "spring")
 interface SurveyOptionMapper {
