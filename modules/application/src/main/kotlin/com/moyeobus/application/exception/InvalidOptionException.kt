95f993e10bc5745c6b7bda2be65da744665734ad
Jinyoung <glitt5384@naver.com>
2025-11-05 16:48:44 +0900

    feat: INVALID_SURVEY_OPTION, INVALID_SURVEY_SPOT 상태 제거 후 메시지로 설명

--- modules/application/src/main/kotlin/com/moyeobus/application/exception/InvalidOptionException.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/exception/InvalidOptionException.kt
@@ -3,4 +3,6 @@ package com.moyeobus.application.exception
 import com.moyeobus.global.response.exception.GlobalException
 import com.moyeobus.global.response.status.ErrorStatus
 
-class InvalidOptionException(id: Long) : GlobalException(ErrorStatus.INVALID_SURVEY_OPTION.withDetail(id.toString()))
+class InvalidOptionException(id: Long) : GlobalException(
+    ErrorStatus.NOT_FOUND.withDetail("설문 옵션(id=$id)을 찾을 수 없습니다.")
+)
--- modules/application/src/main/kotlin/com/moyeobus/application/exception/InvalidSpotException.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/exception/InvalidSpotException.kt
@@ -3,4 +3,6 @@ package com.moyeobus.application.exception
 import com.moyeobus.global.response.exception.GlobalException
 import com.moyeobus.global.response.status.ErrorStatus
 
-class InvalidSpotException(id: Long) : GlobalException(ErrorStatus.INVALID_SURVEY_SPOT.withDetail(id.toString()))
+class InvalidSpotException(id: Long) : GlobalException(
+    ErrorStatus.NOT_FOUND.withDetail("정류장(id=$id)을 찾을 수 없습니다.")
+)
