ad7919652f7b8740d1943dff71a4fccd548abee2
Jinyoung <glitt5384@naver.com>
2025-11-05 16:33:56 +0900

    feat: 커스텀 예외 적용

--- modules/application/src/main/kotlin/com/moyeobus/application/survey/service/SurveyService.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/survey/service/SurveyService.kt
@@ -1,5 +1,7 @@
 package com.moyeobus.application.survey.service
 
+import com.moyeobus.application.exception.InvalidOptionException
+import com.moyeobus.application.exception.InvalidSpotException
 import com.moyeobus.application.survey.port.`in`.SurveyAnswerUseCase
 import com.moyeobus.application.survey.port.`in`.SurveyCommand
 import com.moyeobus.application.survey.port.`in`.SurveyOptionResult
@@ -22,18 +24,24 @@ class SurveyService(
         val departureId = command.departureId
         val destinationId = command.destinationId
 
-        if(!surveyOptionRepository.checkExists(optionId)) {
-            throw IllegalArgumentException("Contain invalid option id")
+
+        if (!surveyOptionRepository.checkExists(optionId)) {
+            throw InvalidOptionException(optionId)
         }
 
-        if (localGovRepository.checkExists(departureId) && localGovRepository.checkExists(destinationId)) {
-            val answer = SurveyAnswer(null, command.departureId,
-                command.departureId, command.optionId)
-            surveyAnswerRepository.save(answer)
-        } else {
-            throw IllegalArgumentException("Contain invalid spot id")
+        // TODO: address 로 localgov 찾는 단계 추가.
+        if (!localGovRepository.checkExists(departureId)) {
+            throw InvalidSpotException(departureId)
         }
 
+        if (!localGovRepository.checkExists(destinationId)) {
+            throw InvalidSpotException(destinationId)
+        }
+
+        val answer = SurveyAnswer(null, command.departureId,
+            command.departureId, command.optionId)
+
+        surveyAnswerRepository.save(answer)
     }
 
     override fun queryAll(): SurveyOptionResult {
