e53ef6f8f09ef12d1b50b09dcf41d2d94196b6f1
Jinyoung <glitt5384@naver.com>
2025-11-05 16:53:03 +0900

    fix: address 테이블에서 조회 하는 것으로 수정

--- modules/application/src/main/kotlin/com/moyeobus/application/survey/service/SurveyService.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/survey/service/SurveyService.kt
@@ -1,5 +1,6 @@
 package com.moyeobus.application.survey.service
 
+import com.moyeobus.application.address.port.out.AddressOutPort
 import com.moyeobus.application.exception.InvalidOptionException
 import com.moyeobus.application.exception.InvalidSpotException
 import com.moyeobus.application.survey.port.`in`.SurveyAnswerUseCase
@@ -8,13 +9,12 @@ import com.moyeobus.application.survey.port.`in`.SurveyOptionResult
 import com.moyeobus.application.survey.port.`in`.SurveyOptionUseCase
 import com.moyeobus.application.survey.port.out.SurveyAnswerOutPort
 import com.moyeobus.application.survey.port.out.SurveyOptionOutPort
-import com.moyeobus.application.user.port.out.LocalGovernmentOutPort
 import com.moyeobus.domain.survey.SurveyAnswer
 import org.springframework.stereotype.Service
 
 @Service
 class SurveyService(
-    private val localGovRepository: LocalGovernmentOutPort,
+    private val addressRepository: AddressOutPort,
     private val surveyOptionRepository: SurveyOptionOutPort,
     private val surveyAnswerRepository: SurveyAnswerOutPort
 ) : SurveyAnswerUseCase, SurveyOptionUseCase {
@@ -29,17 +29,16 @@ class SurveyService(
             throw InvalidOptionException(optionId)
         }
 
-        // TODO: address 로 localgov 찾는 단계 추가.
-        if (!localGovRepository.checkExists(departureId)) {
+        if (!addressRepository.checkExists(departureId)) {
             throw InvalidSpotException(departureId)
         }
 
-        if (!localGovRepository.checkExists(destinationId)) {
+        if (!addressRepository.checkExists(destinationId)) {
             throw InvalidSpotException(destinationId)
         }
 
         val answer = SurveyAnswer(null, command.departureId,
-            command.departureId, command.optionId)
+            command.destinationId, command.optionId)
 
         surveyAnswerRepository.save(answer)
     }
