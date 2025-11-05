3b052319df939b8a92ea95bb339efbceb900e396
Jinyoung <glitt5384@naver.com>
2025-11-05 16:52:07 +0900

    feat: 존재 유무 확인

--- modules/application/src/main/kotlin/com/moyeobus/application/address/port/out/AddressOutPort.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/address/port/out/AddressOutPort.kt
@@ -3,5 +3,6 @@ package com.moyeobus.application.address.port.out
 import com.moyeobus.domain.route.Address
 
 interface AddressOutPort {
+    fun checkExists(id: Long) : Boolean
     fun findAll() : List<Address>
 }
