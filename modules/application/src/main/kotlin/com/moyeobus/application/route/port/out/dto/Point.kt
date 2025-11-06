e2fc7adeef287ec3a21fbc5baf8d6522d682e6cf
Jinyoung <glitt5384@naver.com>
2025-11-06 23:57:51 +0900

    feat: 카카오 모빌리티 길찾기 API에 필요한 요청/응답 객체

---
+++ modules/application/src/main/kotlin/com/moyeobus/application/route/port/out/dto/Point.kt
@@ -0,0 +1,6 @@
+package com.moyeobus.application.route.port.out.dto
+
+data class Point(
+    val x: String,
+    val y: String
+)
---
+++ modules/application/src/main/kotlin/com/moyeobus/application/route/port/out/dto/PointWithAngle.kt
@@ -0,0 +1,7 @@
+package com.moyeobus.application.route.port.out.dto
+
+data class PointWithAngle(
+    val x: String,
+    val y: String,
+    val angle: Int
+)
