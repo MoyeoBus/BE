0dced910d795c102388c9fc1cf9bf0dcdd032f5a
Jinyoung <glitt5384@naver.com>
2025-11-06 23:53:41 +0900

    feat: 지자체에 이름 대신 Area 관계 추가

--- modules/domain/src/main/kotlin/com/moyeobus/domain/user/LocalGovernment.kt
+++ modules/domain/src/main/kotlin/com/moyeobus/domain/user/LocalGovernment.kt
@@ -1,7 +1,13 @@
 package com.moyeobus.domain.user
 
+import com.moyeobus.domain.route.Area
+
 data class LocalGovernment(
     var id: Long? = null,
+
+    val area: Area,
+
     var loginId: String,
+
     var govName: String
 )
