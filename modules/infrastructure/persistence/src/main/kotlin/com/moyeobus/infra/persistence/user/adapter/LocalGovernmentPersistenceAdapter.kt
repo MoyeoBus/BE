3d2d45f0bace686c843155513bff7e2dc991559f
Jinyoung <glitt5384@naver.com>
2025-11-06 23:53:56 +0900

    feat: mapper로 엔티티-도메인 간 변환

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/user/adapter/LocalGovernmentPersistenceAdapter.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/user/adapter/LocalGovernmentPersistenceAdapter.kt
@@ -3,30 +3,25 @@ package com.moyeobus.infra.persistence.user.adapter
 import com.moyeobus.application.user.port.out.LocalGovernmentOutPort
 import com.moyeobus.domain.user.LocalGovernment
 import com.moyeobus.infra.exception.NotFoundException
+import com.moyeobus.infra.persistence.address.mapper.LocalGovernmentMapper
 import com.moyeobus.infra.persistence.user.entity.LocalGovernmentEntity
 import com.moyeobus.infra.persistence.user.repository.LocalGovernmentJpaRepository
 import org.springframework.stereotype.Component
 
 @Component
-// TODO : Mapper로 변환
 class LocalGovernmentPersistenceAdapter(
+    private val mapper: LocalGovernmentMapper,
     private val repo: LocalGovernmentJpaRepository
 ) : LocalGovernmentOutPort {
     override fun findById(id: Long): LocalGovernment {
         val res = repo.findById(id).
                 orElseThrow { NotFoundException("LocalGovernment(id=$id)") }
 
-        return res.toDomain()
+        return mapper.toDomain(res)
     }
 
     override fun checkExists(id: Long): Boolean {
         return repo.existsById(id)
     }
 
-    private fun LocalGovernmentEntity.toDomain() =
-        LocalGovernment(
-            id = this.id,
-            loginId = this.loginId,
-            govName = this.govName
-        )
 }
