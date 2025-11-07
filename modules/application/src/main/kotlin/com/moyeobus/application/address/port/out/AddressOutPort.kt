01a0cbf1090b59b33646591e765c498fda4a8caa
Jinyoung <glitt5384@naver.com>
2025-11-07 19:07:48 +0900

    feat: area로 정류장 반환하는 함수 추가

--- modules/application/src/main/kotlin/com/moyeobus/application/address/port/out/AddressOutPort.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/address/port/out/AddressOutPort.kt
@@ -1,8 +1,11 @@
 package com.moyeobus.application.address.port.out
 
+import com.moyeobus.application.address.dto.SpotDto
 import com.moyeobus.domain.route.Address
+import com.moyeobus.domain.route.Area
 
 interface AddressOutPort {
     fun checkExists(id: Long) : Boolean
     fun findAll() : List<Address>
+    fun findByArea(area: Area) : List<SpotDto>
 }
