3c213e3b4fd9c8f9d93af5fc0d971b269fb28d62
Jinyoung <glitt5384@naver.com>
2025-11-07 19:07:42 +0900

    feat: area로 정류장 반환 쿼리

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/address/repotiory/AddressJpaRepository.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/address/repotiory/AddressJpaRepository.kt
@@ -1,9 +1,21 @@
 package com.moyeobus.infra.persistence.address.repotiory
 
+import com.moyeobus.application.address.dto.SpotDto
 import com.moyeobus.infra.persistence.address.entity.AddressEntity
+import com.moyeobus.infra.persistence.address.entity.AreaEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AddressJpaRepository : JpaRepository<AddressEntity, Long>{
+interface AddressJpaRepository : JpaRepository<AddressEntity, Long> {
+
+    @Query(
+        """
+           SELECT new com.moyeobus.application.address.dto.SpotDto(a.id, a.name, a.lat, a.lon, a.postCode)
+           FROM AddressEntity a
+           WHERE a.area = :area
+        """
+    )
+    fun findByArea(area: AreaEntity) : List<SpotDto>
 }
