fb09cac3501d59e5c7d0cdfc265f4342a428c2f9
Jinyoung <glitt5384@naver.com>
2025-11-07 19:08:28 +0900

    feat: 지역 검색 쿼리 추가

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/address/adapter/AreaPersistenceAdapter.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/address/adapter/AreaPersistenceAdapter.kt
@@ -2,26 +2,19 @@ package com.moyeobus.infra.persistence.address.adapter
 
 import com.moyeobus.application.address.port.out.AreaOutPort
 import com.moyeobus.domain.route.Area
-import com.moyeobus.infra.persistence.address.entity.AreaEntity
+import com.moyeobus.infra.exception.NotFoundException
+import com.moyeobus.infra.persistence.address.mapper.AreaMapper
 import com.moyeobus.infra.persistence.address.repotiory.AreaJpaRepository
 import org.springframework.stereotype.Component
 
 @Component
 class AreaPersistenceAdapter(
+    private val mapper: AreaMapper,
+    private val repo: AreaJpaRepository
 ) : AreaOutPort {
-
-    private fun Area.toEntity() =
-        AreaEntity(
-            id = this.id,
-            sigunguName = this.sigunguName,
-            parentSigunguId = this.parentSigunguId
-        )
-
-    private fun Area.toDomain() =
-        Area(
-            id = this.id,
-            sigunguName = this.sigunguName,
-            parentSigunguId = this.parentSigunguId
-        )
-
+    override fun findById(id: Long) : Area{
+        val res = repo.findById(id).orElseThrow(
+            { NotFoundException("Area(id=$id)") })
+        return mapper.toDomain(res)
+    }
 }
