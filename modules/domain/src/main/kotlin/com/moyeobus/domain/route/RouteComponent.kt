3794e1dc4db8b5d678b0fb59bf292351cb2b2c14
Jinyoung <glitt5384@naver.com>
2025-11-08 15:22:58 +0900

    fix: 경유지도 노선에 포함되기 때문에 Address 대신 위도 경도로 표현

--- modules/domain/src/main/kotlin/com/moyeobus/domain/route/RouteComponent.kt
+++ modules/domain/src/main/kotlin/com/moyeobus/domain/route/RouteComponent.kt
@@ -1,17 +1,23 @@
 package com.moyeobus.domain.route
 
-import java.time.Instant
+import java.time.LocalDateTime
 
 data class RouteComponent (
     val id: Long? = null,
 
     var route: Route?,
 
-    val spot: Address,
+    val location: GeoPoint,
 
-    val assignedTime: Instant
+    val assignedTime: LocalDateTime
 ) {
     fun assignRoute(route: Route) {
         this.route = route
     }
 }
+
+
+data class GeoPoint(
+    val lat: Double,
+    val lon: Double
+)
