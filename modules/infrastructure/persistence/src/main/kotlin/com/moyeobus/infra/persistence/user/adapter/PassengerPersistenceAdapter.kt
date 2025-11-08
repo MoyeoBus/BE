91b743310dde71e93dca7c4e2fe152f665fb0282
Jinyoung <glitt5384@naver.com>
2025-11-08 18:33:25 +0900

    feat: 승객 영속성 트랜잭션

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/user/adapter/PassengerPersistenceAdapter.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/user/adapter/PassengerPersistenceAdapter.kt
@@ -1,8 +1,21 @@
 package com.moyeobus.infra.persistence.user.adapter
 
 import com.moyeobus.application.user.port.out.PassengerOutPort
+import com.moyeobus.domain.user.Passenger
+import com.moyeobus.infra.exception.NotFoundException
+import com.moyeobus.infra.persistence.user.mapper.PassengerMapper
+import com.moyeobus.infra.persistence.user.repository.PassengerJpaRepository
 import org.springframework.stereotype.Component
 
 @Component
-class PassengerPersistenceAdapter : PassengerOutPort {
+class PassengerPersistenceAdapter(
+    private val mapper: PassengerMapper,
+    private val repo: PassengerJpaRepository
+) : PassengerOutPort {
+    override fun findById(id: Long): Passenger {
+        val res = repo.findById(id).orElseThrow(
+            { NotFoundException("Passenger(id=$id)")}
+        )
+        return mapper.toDomain(res)
+    }
 }
