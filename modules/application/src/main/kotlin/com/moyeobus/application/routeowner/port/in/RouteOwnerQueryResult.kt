50172a93c5e30bb4d442e34c886057e8a698f37d
Jinyoung <glitt5384@naver.com>
2025-11-11 07:00:11 +0900

    feat: 응답 객체 items 수정

--- modules/application/src/main/kotlin/com/moyeobus/application/routeowner/port/in/RouteOwnerQueryResult.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/routeowner/port/in/RouteOwnerQueryResult.kt
@@ -1,9 +1,9 @@
 package com.moyeobus.application.routeowner.port.`in`
 
-import com.moyeobus.application.routeowner.port.dto.PassengerRouteDto
+import com.moyeobus.application.route.port.out.RouteInfoDto
 
 data class RouteOwnerQueryResult (
-    val items: List<PassengerRouteDto>,
+    val items: List<RouteInfoDto>,
     val nextCursor: String?,
     val hasNext: Boolean,
 )
