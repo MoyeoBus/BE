b78b0cfe97a27970cba34517e17502bd25a1c4ce
Jinyoung <glitt5384@naver.com>
2025-11-11 07:00:36 +0900

    feat: 쿼리 함수에 dto 목록 반환 로직 추가

--- modules/application/src/main/kotlin/com/moyeobus/application/routeowner/port/service/RouteOwnerQueryService.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/routeowner/port/service/RouteOwnerQueryService.kt
@@ -1,6 +1,9 @@
 package com.moyeobus.application.routeowner.port.service
 
 import com.moyeobus.application.route.port.`in`.QueryFilter
+import com.moyeobus.application.route.port.out.RouteComponentOutPort
+import com.moyeobus.application.route.port.out.RouteInfoDto
+import com.moyeobus.application.route.port.out.RouteOutPort
 import com.moyeobus.application.routeowner.port.`in`.RouteOwnerQueryResult
 import com.moyeobus.application.routeowner.port.`in`.RouteOwnerQueryUseCase
 import com.moyeobus.application.routeowner.port.out.PassengerRouteOutPort
@@ -13,18 +16,28 @@ import java.time.ZoneOffset
 @Service
 class RouteOwnerQueryService(
     private val cursorUtil: CursorUtil,
-    private val passengerRouteRepository: PassengerRouteOutPort
+    private val passengerRouteRepository: PassengerRouteOutPort,
+    private val routeComponentRepository: RouteComponentOutPort,
+    private val routeRepository: RouteOutPort
 ) : RouteOwnerQueryUseCase {
+
     override fun query(id: Long, filter: QueryFilter): RouteOwnerQueryResult {
         val decodedCursor = cursorUtil.decode(filter.cursor)
         val routeOwnerQuery = RouteOwnerQuery.from(filter, decodedCursor)
+
         val queryItems = passengerRouteRepository.findBy(routeOwnerQuery)
+        val routes = queryItems.items.map { it.route }
+
+        val dtoList: List<RouteInfoDto> = routes.map { route ->
+            routeComponentRepository.findById(route.id!!)
+        }
+
 
         val nextCursorCreatedAt = queryItems.nextCursorCreatedAt
             ?.toInstant(ZoneOffset.UTC)
 
         return RouteOwnerQueryResult(
-            items = queryItems.items,
+            items = dtoList,
             nextCursor = if (queryItems.hasNext) {
                 cursorUtil.encode(CursorWrapper(nextCursorCreatedAt, queryItems.nextCursorId))
             } else null,
