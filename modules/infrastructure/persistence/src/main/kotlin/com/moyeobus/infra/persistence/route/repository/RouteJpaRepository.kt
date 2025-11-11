f41ec2a2df5ab912b52047a8c6509aed0b67da43
Jinyoung <glitt5384@naver.com>
2025-11-11 23:56:24 +0900

    feat: 노선 상태값 조회

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/route/repository/RouteJpaRepository.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/route/repository/RouteJpaRepository.kt
@@ -2,8 +2,17 @@ package com.moyeobus.infra.persistence.route.repository
 
 import com.moyeobus.infra.persistence.route.entity.RouteEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface RouteJpaRepository : JpaRepository<RouteEntity, Long> {
+
+    @Query("""
+        SELECT r.status
+        FROM RouteEntity r
+        Where r.id = :id
+    """)
+    fun findStatus(id: Long) : String
+
 }
