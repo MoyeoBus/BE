9926518d633fccdd37b050e50c75f105326173de
Jinyoung <glitt5384@naver.com>
2025-11-12 23:49:15 +0900

    feat: findById 추가

---
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/localgov/adapter/LocalGovernmentPersistenceAdapter.kt
@@ -0,0 +1,19 @@
+package com.moyeobus.infra.persistence.localgov.adapter
+
+import com.moyeobus.application.localgov.port.out.LocalGovernmentOutPort
+import com.moyeobus.domain.user.LocalGovernment
+import com.moyeobus.infra.exception.NotFoundException
+import com.moyeobus.infra.persistence.address.mapper.LocalGovernmentMapper
+import com.moyeobus.infra.persistence.localgov.repository.LocalGovernmentJpaRepository
+import org.springframework.stereotype.Component
+
+@Component
+class LocalGovernmentPersistenceAdapter(
+    private val mapper: LocalGovernmentMapper,
+    private val repo: LocalGovernmentJpaRepository
+) : LocalGovernmentOutPort {
+    override fun findById(id: Long): LocalGovernment {
+        val res = repo.findById(id).orElseThrow({ NotFoundException("LocalGovernment(id=$id)")})
+        return mapper.toDomain(res)
+    }
+}
