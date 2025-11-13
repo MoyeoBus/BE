f7513de1b3d8470c54d7b05c760550e72371cad4
Jinyoung <glitt5384@naver.com>
2025-11-13 23:43:45 +0900

    feat: 지자체 조회 유스케이스

--- modules/application/src/main/kotlin/com/moyeobus/application/localgov/port/out/LocalGovernmentUseCase.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/localgov/port/out/LocalGovernmentUseCase.kt
@@ -1,7 +1,15 @@
 package com.moyeobus.application.localgov.port.out
 
-import com.moyeobus.domain.route.RouteRequest
+import com.moyeobus.application.localgov.port.`in`.LocalGovDateResult
+import com.moyeobus.application.localgov.port.`in`.LocalGovRouteResult
+import com.moyeobus.application.localgov.port.`in`.LocalGovStatusResult
+import com.moyeobus.application.localgov.port.`in`.LocalGovTimeResult
+import java.time.LocalDate
+
 
 interface LocalGovernmentUseCase {
-    fun queryLocal(id: Long) : Any
+    fun queryLocal(id: Long) : LocalGovStatusResult
+    fun queryDate(id: Long) : LocalGovDateResult
+    fun queryHour(id: Long, date: LocalDate) : LocalGovTimeResult
+    fun queryRoute(id: Long) : LocalGovRouteResult
 }
