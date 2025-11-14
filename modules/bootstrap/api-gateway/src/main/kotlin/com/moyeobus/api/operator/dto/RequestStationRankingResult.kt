c42441e115cd766c4c92260e0844ff38f7ba1c84
Jinyoung <glitt5384@naver.com>
2025-11-14 13:38:19 +0900

    feat: 노선 정거장 순위 데이터 응답 객체

---
+++ modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/operator/dto/RequestStationRankingResult.kt
@@ -0,0 +1,8 @@
+package com.moyeobus.api.operator.dto
+
+import com.moyeobus.application.route.model.RequestStationRanking
+
+data class RequestStationRankingResult(
+    val items: List<RequestStationRanking>
+)
+
