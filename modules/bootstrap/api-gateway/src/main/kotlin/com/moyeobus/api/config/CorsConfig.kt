c6f59d6d7b0de6e7b66c377b65b5ecba28e53f6a
Jinyoung <glitt5384@naver.com>
2025-11-14 16:27:06 +0900

    feat: cors 설정 추가

---
+++ modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/config/CorsConfig.kt
@@ -0,0 +1,25 @@
+package com.moyeobus.api.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsFilter(): CorsWebFilter {
+        val config = CorsConfiguration().apply {
+            allowedOrigins = listOf("*")
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+        }
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/api/**", config)
+
+        return CorsWebFilter(source)
+    }
+}
