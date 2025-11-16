0fed46513b62bcfb3fa39f264c134b440976a079
Jinyoung <glitt5384@naver.com>
2025-11-16 23:41:29 +0900

    chore: 응답 객체명 수정

--- modules/application/src/main/kotlin/com/moyeobus/application/route/port/in/RouteQueryUseCase.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/route/port/in/RouteQueryUseCase.kt
@@ -1,9 +1,8 @@
 package com.moyeobus.application.route.port.`in`
 
 import com.moyeobus.application.route.model.RouteDetail
-import com.moyeobus.application.routeowner.port.`in`.RouteOwnerQueryResult
 
 interface RouteQueryUseCase {
     fun queryRouteDetail(id: Long) : RouteDetail
-    fun queryLocalRoute(filter: LocalQueryFilter) : RouteOwnerQueryResult
+    fun queryLocalRoute(filter: LocalQueryFilter) : LocalRouteQueryResult
 }
