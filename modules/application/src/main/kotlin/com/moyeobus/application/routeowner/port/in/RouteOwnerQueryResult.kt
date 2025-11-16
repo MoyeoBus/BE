721ca87c0c02b89a1663123624d4bde368947528
Jinyoung <glitt5384@naver.com>
2025-11-16 23:32:39 +0900

    chore: RouteInfo -> PassengerRouteInfo

--- modules/application/src/main/kotlin/com/moyeobus/application/routeowner/port/in/RouteOwnerQueryResult.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/routeowner/port/in/RouteOwnerQueryResult.kt
@@ -1,9 +1,9 @@
 package com.moyeobus.application.routeowner.port.`in`
 
-import com.moyeobus.application.routeowner.port.dto.RouteInfoDto
+import com.moyeobus.application.routeowner.port.dto.PassengerRouteInfo
 
 data class RouteOwnerQueryResult (
-    val items: List<RouteInfoDto>,
+    val items: List<PassengerRouteInfo>,
     val nextCursor: String?,
     val hasNext: Boolean,
 )
