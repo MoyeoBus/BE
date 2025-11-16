35eecb7f8f3496d00dad80a82430b4e6a5975d3c
Jinyoung <glitt5384@naver.com>
2025-11-17 08:36:31 +0900

    feat: 운행관리 페이지에 필요한 데이터 쿼리

--- modules/application/src/main/kotlin/com/moyeobus/application/localgov/port/in/LocalGovernmentUseCase.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/localgov/port/in/LocalGovernmentUseCase.kt
@@ -1,5 +1,8 @@
 package com.moyeobus.application.localgov.port.`in`
 
+import com.moyeobus.application.route.model.RouteTrackInfo
+import com.moyeobus.application.route.model.TrackItemOutput
+import com.moyeobus.domain.route.GeoPoint
 import java.time.LocalDate
 import java.time.YearMonth
 
@@ -10,4 +13,7 @@ interface LocalGovernmentUseCase {
     fun queryRoute(id: Long) : LocalGovRouteResult
     fun queryDeparture(routeId: Long) : List<LocalGovStationWrapper>
     fun queryDestination(routeId: Long) : List<LocalGovStationWrapper>
+    fun queryRouteTrackInfos(routeId: Long, currentStation: String) : RouteTrackInfo
+    fun queryRouteTrackItems(routeId: Long) : List<TrackItemOutput>
+    fun queryRouteTrackPoints(routeId: Long) : List<GeoPoint>
 }
