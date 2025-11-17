4437c76d34d9e443598ec3e44e1b9f780923ec78
Jinyoung <glitt5384@naver.com>
2025-11-17 12:29:11 +0900

    feat: 설문 결과 집계

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/survey/repository/SurveyAnswerJpaRepository.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/survey/repository/SurveyAnswerJpaRepository.kt
@@ -1,9 +1,40 @@
 package com.moyeobus.infra.persistence.survey.repository
 
+import com.moyeobus.infra.persistence.survey.dto.SurveyProjection
 import com.moyeobus.infra.persistence.survey.entity.SurveyAnswerEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface SurveyAnswerJpaRepository : JpaRepository<SurveyAnswerEntity, Long> {
+
+    @Query(
+        """
+    SELECT 
+        o.reason AS reason,
+        COUNT(a) AS count,
+        CONCAT(
+            ROUND(
+                (COUNT(a) * 100.0) /
+                (SELECT COUNT(a2)
+                 FROM SurveyAnswerEntity a2
+                 WHERE a2.departureLocalGovId = :localGovId
+                    OR a2.destinationLocalGovId = :localGovId),
+                1
+            ),
+            '%'
+        ) AS ratio
+    FROM SurveyAnswerEntity a
+    JOIN SurveyOptionEntity o
+      ON a.optionId = o.id
+    WHERE a.departureLocalGovId = :localGovId
+       OR a.destinationLocalGovId = :localGovId
+    GROUP BY o.reason
+    ORDER BY count DESC
+    """
+    )
+    fun countByLocalGovId(@Param("localGovId") localGovId: Long): List<SurveyProjection>
+
 }
