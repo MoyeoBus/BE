70eed443af8a19c7329c67cc58f5269a527a061e
Jinyoung <glitt5384@naver.com>
2025-11-17 10:15:52 +0900

    fix: 운수사에 필요한 조회 기능 이동

--- modules/application/src/main/kotlin/com/moyeobus/application/localgov/port/in/LocalGovernmentUseCase.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/localgov/port/in/LocalGovernmentUseCase.kt
@@ -1,5 +1,6 @@
 package com.moyeobus.application.localgov.port.`in`
 
+import com.moyeobus.application.route.model.BusUsageCount
 import com.moyeobus.application.route.model.RouteTrackInfo
 import com.moyeobus.application.route.model.TrackItemOutput
 import com.moyeobus.domain.route.GeoPoint
@@ -13,7 +14,4 @@ interface LocalGovernmentUseCase {
     fun queryRoute(id: Long) : LocalGovRouteResult
     fun queryDeparture(routeId: Long) : List<LocalGovStationWrapper>
     fun queryDestination(routeId: Long) : List<LocalGovStationWrapper>
-    fun queryRouteTrackInfos(routeId: Long, currentStation: String) : RouteTrackInfo
-    fun queryRouteTrackItems(routeId: Long) : List<TrackItemOutput>
-    fun queryRouteTrackPoints(routeId: Long) : List<GeoPoint>
 }
