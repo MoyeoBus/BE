566f0d4cf8cdfba49e97401336d92d078deeba5f
Jinyoung <glitt5384@naver.com>
2025-11-18 10:30:31 +0900

    feat: 추적 정보에 다음 정류장 위도 경도 포함

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/route/dto/TrackInfoProjection.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/route/dto/TrackInfoProjection.kt
@@ -1,8 +1,11 @@
 package com.moyeobus.infra.persistence.route.dto
 
+
 interface TrackInfoProjection {
     val routeId: Long
-    val nextStation: String
-    val gapTime: Int
-    val remainDistance: Int
+    val nextStation: String?
+    val nextLat: Double?
+    val nextLon: Double?
+    val gapTime: Int?
+    val remainDistance: Int?
 }
