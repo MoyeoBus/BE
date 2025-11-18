d03b22254caba6845a9d8b8bb2801d7faec14d40
Jinyoung <glitt5384@naver.com>
2025-11-18 22:16:45 +0900

    feat: 정류장 조회 api 요청 파라미터 수정

--- modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/address/AddressController.kt
+++ modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/address/AddressController.kt
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,8 +17,11 @@ class AddressController(
     private val addressQueryService: AddressQueryUseCase
 ) : AddressControllerDocs {
 
-    @GetMapping("/{sigunguId}")
-    override fun getStations(@PathVariable sigunguId: Long) : ResponseEntity<ApiResponse<List<StationDto>>> {
-        return ResponseEntity.ok(ApiResponse.onSuccess(addressQueryService.queryStations(sigunguId)))
+    @GetMapping
+    override fun getStations(
+        @RequestParam dosi: String,
+        @RequestParam sigungu: String
+    ) : ResponseEntity<ApiResponse<List<StationDto>>> {
+        return ResponseEntity.ok(ApiResponse.onSuccess(addressQueryService.queryStations(dosi, sigungu)))
     }
 }
