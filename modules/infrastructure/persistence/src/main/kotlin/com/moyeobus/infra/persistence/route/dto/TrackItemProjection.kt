f245d07e248e6ef6a1197013781916993d5ae112
Jinyoung <glitt5384@naver.com>
2025-11-18 10:30:39 +0900

    feat: 추적 정보에 다음 정류장 위도 경도 포함

--- modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/route/dto/TrackItemProjection.kt
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/persistence/route/dto/TrackItemProjection.kt
@@ -2,6 +2,8 @@ package com.moyeobus.infra.persistence.route.dto
 
 interface TrackItemProjection {
     val station: String
+    val lat: Double
+    val lon: Double
     val time: String
     val tag: String
 }
