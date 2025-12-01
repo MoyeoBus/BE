3548400103194e785e19ffe33f4a4fcabbc54ff9
Jinyoung <glitt5384@naver.com>
2025-12-01 23:44:44 +0900

    feat: 자체 로그인 api 노출

--- modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/auth/LoginController.kt
+++ modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/auth/LoginController.kt
@@ -1,6 +1,9 @@
 package com.moyeobus.api.auth
 
+import com.moyeobus.application.auth.port.`in`.LoginCommand
+import com.moyeobus.application.auth.port.`in`.LoginUseCase
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -8,7 +11,17 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/login")
-class LoginController {
+class LoginController(
+    private val loginService: LoginUseCase
+) {
+
+    @GetMapping
+    fun login(
+        @Validated request: LoginCommand
+    ){
+
+    }
+
 
     @GetMapping("/oauth")
     fun redirectToProvider(
