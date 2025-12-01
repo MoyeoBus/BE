445b0931314c71d4e4aa37670a89c473a65d37f1
Jinyoung <glitt5384@naver.com>
2025-12-01 23:45:08 +0900

    feat: 이메일로 승객 조회

--- modules/application/src/main/kotlin/com/moyeobus/application/user/port/out/PassengerOutPort.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/user/port/out/PassengerOutPort.kt
@@ -4,4 +4,6 @@ import com.moyeobus.domain.user.Passenger
 
 interface PassengerOutPort {
     fun findById(id: Long) : Passenger
+    fun findByEmail(email: String) : Passenger
+    fun matches(raw: String, encoded: String) : Boolean
 }
