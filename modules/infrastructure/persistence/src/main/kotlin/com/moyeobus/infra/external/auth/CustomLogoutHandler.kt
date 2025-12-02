07e696a449601c7930567ad2ef24d445f4f50f88
Jinyoung <glitt5384@naver.com>
2025-12-02 19:01:36 +0900

    feat: 커스텀 로그아웃 핸들러

---
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/external/auth/CustomLogoutHandler.kt
@@ -0,0 +1,38 @@
+package com.moyeobus.infra.external.auth
+
+import com.moyeobus.infra.external.oauth2.util.CookieUtil
+import com.moyeobus.infra.external.oauth2.util.CookieUtil.expireCookie
+import com.moyeobus.infra.external.oauth2.util.JwtUtil
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.logout.LogoutHandler
+import org.springframework.stereotype.Component
+
+@Component
+class CustomLogoutHandler(
+    private val tokenBlackListService: TokenBlackListService,
+    private val jwtUtil: JwtUtil
+) : LogoutHandler {
+
+    override fun logout(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authentication: Authentication?
+    ) {
+        val refreshToken = CookieUtil.getRefreshTokenFromRequest(request)
+
+        refreshToken?.let {
+            tokenBlackListService.addRefreshTokenBlackList(
+                it,
+                jwtUtil.getRefreshTokenExpireTime(it)
+            )
+
+            expireCookie(response, "refresh", "/", true, true, "None")
+            expireCookie(response, "access", "/", true, true, "None")
+        }
+
+        SecurityContextHolder.clearContext()
+    }
+}
