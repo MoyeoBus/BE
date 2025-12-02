e2bbd9d7acf0d36c37600b7940f892db3105c27c
Jinyoung <glitt5384@naver.com>
2025-12-02 19:02:53 +0900

    feat: 로그인 필터

---
+++ modules/infrastructure/persistence/src/main/kotlin/com/moyeobus/infra/external/auth/LoginFilter.kt
@@ -0,0 +1,90 @@
+package com.moyeobus.infra.external.auth
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moyeobus.application.auth.port.`in`.LoginCommand
+import com.moyeobus.infra.exception.NotFoundException
+import com.moyeobus.infra.external.auth.security.CustomUserDetails
+import com.moyeobus.infra.external.oauth2.util.CookieUtil
+import com.moyeobus.infra.external.oauth2.util.JwtUtil
+import com.moyeobus.infra.persistence.user.repository.PassengerJpaRepository
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component
+
+import org.springframework.util.StreamUtils;
+
+
+import java.nio.charset.StandardCharsets;
+
+
+@Component
+class LoginFilter(
+    authenticationManager: AuthenticationManager,
+    private val jwtUtil: JwtUtil,
+    private val cookieUtil: CookieUtil,
+    private val passengerRepository: PassengerJpaRepository
+) : UsernamePasswordAuthenticationFilter() {
+
+    init {
+        super.setAuthenticationManager(authenticationManager)
+        setFilterProcessesUrl("/api/v1/login")
+    }
+
+    override fun attemptAuthentication(
+        request: HttpServletRequest,
+        response: HttpServletResponse
+    ): Authentication {
+
+        return try {
+            val messageBody = StreamUtils.copyToString(request.inputStream, StandardCharsets.UTF_8)
+            val loginRequest = ObjectMapper().readValue(messageBody, LoginCommand::class.java)
+
+            val passenger = passengerRepository.findByEmail(loginRequest.email)?:
+            throw NotFoundException("Passenger(email=$loginRequest.email)")
+
+            if (passenger.userType.toString() != "LOCAL") {
+                throw RuntimeException("${passenger.userType} 계정으로 가입된 회원입니다.")
+            }
+
+            val authToken = UsernamePasswordAuthenticationToken(
+                loginRequest.email,
+                loginRequest.password,
+                null
+            )
+
+            authenticationManager.authenticate(authToken)
+
+        } catch (e: Exception) {
+            throw RuntimeException("Failed to process authentication", e)
+        }
+    }
+
+    override fun successfulAuthentication(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        chain: FilterChain,
+        authentication: Authentication
+    ) {
+        val userDetails = authentication.principal as CustomUserDetails
+        val email = userDetails.email
+
+        try {
+            val access = jwtUtil.createAccess(email)
+            val refresh = jwtUtil.createRefresh(email)
+
+            response.addCookie(cookieUtil.createCookie("access", access))
+            response.addCookie(cookieUtil.createCookie("refresh", refresh))
+            response.status = HttpStatus.OK.value()
+
+        } catch (e: Exception) {
+            response.status = HttpStatus.INTERNAL_SERVER_ERROR.value()
+        }
+    }
+}
