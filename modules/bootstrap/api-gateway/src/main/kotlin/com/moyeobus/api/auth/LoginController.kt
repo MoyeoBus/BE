2d6ce29869e7f68b7be9c2ecbef3d7f5cadef9eb
Jinyoung <glitt5384@naver.com>
2025-12-02 19:48:40 +0900

    feat: 스웨거 문서 적용

--- modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/auth/LoginController.kt
+++ modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/auth/LoginController.kt
@@ -1,19 +1,25 @@
 package com.moyeobus.api.auth
 
-import com.moyeobus.application.auth.port.`in`.SignUpUseCase
+import com.moyeobus.api.docs.LoginControllerDocs
+import com.moyeobus.application.auth.port.`in`.LoginCommand
 import jakarta.servlet.http.HttpServletResponse
+import jakarta.validation.Valid
 
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/login")
-class LoginController() {
+class LoginController() : LoginControllerDocs{
 
-    @GetMapping("/oauth")
-    fun redirectToProvider(
+    @PostMapping
+    override fun localLogin(@Valid @RequestBody request: LoginCommand) {}
+
+    @PostMapping("/oauth")
+    override fun redirectToProvider(
         @RequestParam provider: String,
         response: HttpServletResponse
     ) {
