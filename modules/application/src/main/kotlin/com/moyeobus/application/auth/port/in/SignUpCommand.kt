de256c474741714a512abc49d6ce7fe845207619
Jinyoung <glitt5384@naver.com>
2025-12-02 19:49:00 +0900

    feat: 에러 메시지에 마침표 추가

--- modules/application/src/main/kotlin/com/moyeobus/application/auth/port/in/SignUpCommand.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/auth/port/in/SignUpCommand.kt
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 
 data class SignUpCommand(
-    @field:Email(message = "이메일 형식이 아닙니다")
-    @field:NotBlank(message = "이메일은 필수입니다")
+    @field:Email(message = "이메일 형식이 아닙니다.")
+    @field:NotBlank(message = "이메일은 필수입니다.")
     val email: String,
-    @field:NotBlank(message = "비밀번호는 필수입니다")
+    @field:NotBlank(message = "비밀번호는 필수입니다.")
     @field:Size(min = 7, message = "비밀번호 길이는 최소 7 이상이어야 합니다.")
     val password: String
 )
