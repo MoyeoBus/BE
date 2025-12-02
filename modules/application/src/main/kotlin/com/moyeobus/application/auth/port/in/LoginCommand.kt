b38441761042c21f27c2f162ab07409eb8b8c3c2
Jinyoung <glitt5384@naver.com>
2025-12-02 19:48:24 +0900

    feat: 이메일과 비밀번호에 기본값 설정

--- modules/application/src/main/kotlin/com/moyeobus/application/auth/port/in/LoginCommand.kt
+++ modules/application/src/main/kotlin/com/moyeobus/application/auth/port/in/LoginCommand.kt
@@ -7,8 +7,8 @@ import jakarta.validation.constraints.Size
 data class LoginCommand(
     @field:Email(message = "이메일 형식이 아닙니다")
     @field:NotBlank(message = "이메일은 필수입니다")
-    val email: String,
+    val email: String = "",
     @field:NotBlank(message = "비밀번호는 필수입니다")
     @field:Size(min = 7, message = "비밀번호 길이는 최소 7 이상이어야 합니다.")
-    val password: String
+    val password: String = ""
 )
