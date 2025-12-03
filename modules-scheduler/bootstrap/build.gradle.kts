d7b9305590acf48acd9345b14ddaf7c94b3cefd4
Jinyoung <glitt5384@naver.com>
2025-12-03 19:33:40 +0900

    fix: domain 모듈 제거

--- modules-scheduler/bootstrap/build.gradle.kts
+++ modules-scheduler/bootstrap/build.gradle.kts
@@ -14,7 +14,6 @@ tasks.bootJar {
 dependencies {
     implementation(projects.modulesScheduler.common)
     implementation(projects.modulesScheduler.external)
-    implementation(projects.modulesScheduler.domain)
     implementation(projects.modulesScheduler.application)
     implementation(projects.modulesScheduler.infrastructure.persistence)
     implementation(libs.spring.boot.starter.jpa)
