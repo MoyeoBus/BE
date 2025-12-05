6c82b9cea870f6f8efa62e91878a1585cff82050
Jinyoung <glitt5384@naver.com>
2025-12-05 22:30:49 +0900

    feat: 스케줄링 활성화

--- modules-scheduler/bootstrap/src/main/kotlin/com/moyeobus/scheduler/MoyeoBusSchedulerApplication.kt
+++ modules-scheduler/bootstrap/src/main/kotlin/com/moyeobus/scheduler/MoyeoBusSchedulerApplication.kt
@@ -2,8 +2,10 @@ package com.moyeobus.scheduler
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication(scanBasePackages = ["com.moyeobus.scheduler"])
+@EnableScheduling
 class MoyeoBusSchedulerApplication
 fun main(args: Array<String>) {
 	runApplication<MoyeoBusSchedulerApplication>(*args)
