41e01f98954b8a49f877862cc558bff9b634e1a3
Jinyoung <glitt5384@naver.com>
2025-12-06 02:02:14 +0900

    feat: 로깅 의존성 추가

--- modules-scheduler/application/build.gradle.kts
+++ modules-scheduler/application/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     // Only need Spring annotations (@Service) for this module
     implementation("org.springframework:spring-context")
     implementation("jakarta.validation:jakarta.validation-api:3.1.1")
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.1")
 }
 repositories {
     mavenCentral()
