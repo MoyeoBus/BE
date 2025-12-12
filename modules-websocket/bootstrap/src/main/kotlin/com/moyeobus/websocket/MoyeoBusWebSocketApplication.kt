fc01d0c44f98de739a4e415906e9fd451fdc2195
Jinyoung <glitt5384@naver.com>
2025-12-12 20:05:24 +0900

    feat: 웹소켓 서버 애플리케이션

---
+++ modules-websocket/bootstrap/src/main/kotlin/com/moyeobus/websocket/MoyeoBusWebSocketApplication.kt
@@ -0,0 +1,11 @@
+package com.moyeobus.websocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(scanBasePackages = ["com.moyeobus.websocket"])
+class MoyeoBusWebSocketApplication
+
+fun main(args: Array<String>) {
+    runApplication<MoyeoBusWebSocketApplication>(*args)
+}
