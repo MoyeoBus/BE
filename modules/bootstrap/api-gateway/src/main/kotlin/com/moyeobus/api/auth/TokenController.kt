9caf452a70d344a25051ce23856547a599f6e3fe
Jinyoung <glitt5384@naver.com>
2025-12-23 06:18:47 +0900

    feat: 토큰 컨트롤러

---
+++ modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/auth/TokenController.kt
@@ -0,0 +1,10 @@
+package com.moyeobus.api.auth
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/api/v1/tokens")
+class TokenController {
+}
