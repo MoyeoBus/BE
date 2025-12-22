e8d062a926f58222ba546bdff5baa580d1289580
Jinyoung <glitt5384@naver.com>
2025-12-23 08:57:46 +0900

    feat: 스웨거 문서 구현

--- modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/auth/TokenController.kt
+++ modules/bootstrap/api-gateway/src/main/kotlin/com/moyeobus/api/auth/TokenController.kt
@@ -1,10 +1,14 @@
 package com.moyeobus.api.auth
 
+import com.moyeobus.api.docs.TokenControllerDocs
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 @RequestMapping("/api/v1/tokens")
-class TokenController {
+class TokenController : TokenControllerDocs{
+    @GetMapping
+    override fun reissue() { }
 }
