8e419cc246dbf8380fba13fde3b23d83a22ff32d
Jinyoung <glitt5384@naver.com>
2025-12-27 15:12:48 +0900

    fix: 버스 위치 전송 API의 Http 메서드 수정

--- modules-websocket/bootstrap/src/main/kotlin/com/moyeobus/websocket/controller/SseController.kt
+++ modules-websocket/bootstrap/src/main/kotlin/com/moyeobus/websocket/controller/SseController.kt
@@ -4,7 +4,9 @@ import com.moyeobus.websocket.application.port.`in`.SocketUseCase
 import com.moyeobus.websocket.dto.BusLocationDto
 import org.springframework.http.MediaType
 import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 
 @RestController
 @RequestMapping("/sse")
+@CrossOrigin("http://localhost:63342")
 class SseController(
     private val sseService: SocketUseCase
 ) {
@@ -24,8 +27,8 @@ class SseController(
         return sseService.subscribe(userId)
     }
 
-    @MessageMapping("/bus/location")
-    fun updateLocation(@RequestBody id: BusLocationDto) {
-        println(id)
+    @PostMapping("/bus/location")
+    fun updateLocation(@RequestBody dto: BusLocationDto) {
+        sseService.broadcast(dto)
     }
 }
