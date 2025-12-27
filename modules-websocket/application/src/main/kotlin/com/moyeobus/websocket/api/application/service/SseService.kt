52cec5353986800e7bc29d1784dd2783bc2c899c
Jinyoung <glitt5384@naver.com>
2025-12-27 23:35:40 +0900

    fix: emitter를 List에 담아 한 routeId에 대해 여러 구독자 보유하도록 수정

--- modules-websocket/application/src/main/kotlin/com/moyeobus/websocket/api/application/service/SseService.kt
+++ modules-websocket/application/src/main/kotlin/com/moyeobus/websocket/api/application/service/SseService.kt
@@ -5,27 +5,30 @@ import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArrayList
 
 @Service
 class SseService : SocketUseCase{
-    private val emitters = ConcurrentHashMap<Long, SseEmitter>()
+    private val emitters = ConcurrentHashMap<Long, MutableList<SseEmitter>>()
 
     override fun subscribe(routeId: Long) : SseEmitter {
         val emitter = SseEmitter(Long.MAX_VALUE)
 
-        emitters[routeId] = emitter
+        emitters.computeIfAbsent(routeId) {
+            CopyOnWriteArrayList()
+            }.add(emitter)
 
         // 연결 종료 시 정리
         emitter.onCompletion {
-            emitters.remove(routeId)
+            emitters[routeId]?.remove(emitter)
         }
 
         emitter.onTimeout {
-            emitters.remove(routeId)
+            emitters[routeId]?.remove(emitter)
         }
 
         emitter.onError {
-            emitters.remove(routeId)
+            emitters[routeId]?.remove(emitter)
         }
 
         // 최초 연결 확인용 이벤트 (권장)
@@ -46,16 +49,18 @@ class SseService : SocketUseCase{
      */
     @Async
     override fun sendToUser(routeId: Long, data: Any) {
-        val emitter = emitters[routeId] ?: return
+        val emitterList = emitters[routeId] ?: return
 
-        try {
-            emitter.send(
-                SseEmitter.event()
-                    .name("notification")
-                    .data(data)
-            )
-        } catch (e: Exception) {
-            emitters.remove(routeId)
+        emitterList.forEach { emitter ->
+            try {
+                emitter.send(
+                    SseEmitter.event()
+                        .name("notification")
+                        .data(data)
+                )
+            } catch (e: Exception) {
+                emitterList.remove(emitter)
+            }
         }
     }
 
@@ -64,15 +69,17 @@ class SseService : SocketUseCase{
      */
     @Async
     override fun broadcast(data: Any) {
-        emitters.forEach { (routeId, emitter) ->
-            try {
-                emitter.send(
-                    SseEmitter.event()
-                        .name("broadcast")
-                        .data(data)
-                )
-            } catch (e: Exception) {
-                emitters.remove(routeId)
+        emitters.forEach { (_, emitterList) ->
+            emitterList.forEach { emitter ->
+                try {
+                    emitter.send(
+                        SseEmitter.event()
+                            .name("broadcast")
+                            .data(data)
+                    )
+                } catch (e: Exception) {
+                    emitterList.remove(emitter)
+                }
             }
         }
     }
