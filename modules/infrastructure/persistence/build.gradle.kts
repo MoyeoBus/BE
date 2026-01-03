073f3dcf3fd6586396085b64eb59de9140c461a3
Jinyoung <glitt5384@naver.com>
2026-01-03 19:32:03 +0900

    fix: actuator 의존성 제거

--- modules/infrastructure/persistence/build.gradle.kts
+++ modules/infrastructure/persistence/build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
     implementation(libs.bundles.jwt)
     implementation("com.mysql:mysql-connector-j:8.3.0")
     implementation("org.mapstruct:mapstruct:1.6.2")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation ("org.springframework.boot:spring-boot-starter-webflux")
 
     kapt("org.mapstruct:mapstruct-processor:1.6.2")
